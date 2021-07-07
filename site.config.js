498162bc42932a5246503e0774e9b96282a44fd2
imdigi <87068778+imdigi@users.noreply.github.com>
2021-07-07 18:14:34 +0800

    Update site.config.js

--- site.config.js
+++ site.config.js
@@ -4,12 +4,12 @@ module.exports = {
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
-  rootNotionSpaceId: null,
+  rootNotionSpaceId: 2fe9554e2b174e20baf5e09274bddd34,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'imdigi',
+  domain: 'https://www.notion.so/Blog-2fe9554e2b174e20baf5e09274bddd342fe9554e2b174e20baf5e09274bddd34',
+  author: 'Lee',
 
   // open graph metadata (optional)
   description: 'Example site description',
@@ -17,9 +17,9 @@ module.exports = {
   socialImageSubtitle: 'Hello World! 👋',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  twitter: ' ',
+  github: ' ',
+  linkedin: ' ',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
